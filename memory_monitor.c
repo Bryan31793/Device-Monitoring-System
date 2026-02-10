5a170be0b4f1d1cad8275e8b67bb167f9a862685
Bryan31793 <bryanrmzlop@gmail.com>
2026-02-10 01:00:11 -0600

    changed program architecture

--- memory_monitor.c
+++ memory_monitor.c
@@ -6,50 +6,53 @@
 #include <string.h>
 #include <stdbool.h>
 
-void mem_monitor();
+void father_process();
+void mem_monitor(int *);
+void process_pipe(int *);
 void parser(const char *);
 
 int main() {
-    mem_monitor();
-
+    father_process();
     return 0;
 }
 
-void mem_monitor() {
-    int pipe1[2], pipe2[2];
-    pipe(pipe1);   //create a pipe in the kernel
+void father_process() {
+    int pipe1[2], pipe2[2], pipe3[2];
+    pipe(pipe1);
     pipe(pipe2);
-
-    if(fork() == 0) {   //if child process
-        close(pipe1[0]);   //close read channel
-        dup2(pipe1[1], STDOUT_FILENO); //redirect STDOUT to the pipe's write end
-        close(pipe1[1]);   //close write channel
-
-        execlp("cat", "cat", "/proc/meminfo", NULL);    //execute cat /proc/meminfo
-        perror("Process error");
-        exit(1);
-    }
+    pipe(pipe3);
 
     if(fork() == 0) {
-        close(pipe2[0]);
-        dup2(pipe2[1], STDOUT_FILENO);
-        close(pipe2[1]);
-
-        execlp("cat", "cat", "/proc/stat", NULL);
-        perror("Process error");
-        exit(1);
+        mem_monitor(pipe1);
     }
 
     close(pipe1[1]);   //parent process closes write channel
-    close(pipe2[1]);
+    process_pipe(pipe1);
+    close(pipe1[0]);
+    
+    wait(NULL);
 
+}
 
+void mem_monitor(int *pipe_fd) {
+    
+    close(pipe_fd[0]);   //close read channel
+    dup2(pipe_fd[1], STDOUT_FILENO); //redirect STDOUT to the pipe's write end
+    close(pipe_fd[1]);   //close write channel
+
+    execlp("cat", "cat", "/proc/meminfo", NULL);    //execute cat /proc/meminfo
+    perror("Process error");
+    exit(1);
+    
+}
+
+void process_pipe(int *pipe_fd) {
     char buffer[1024];
     char line[2048];
     int line_len = 0;
     ssize_t n;
 
-    while((n = read(pipe1[0], buffer, sizeof(buffer))) > 0) {
+    while((n = read(pipe_fd[0], buffer, sizeof(buffer))) > 0) {
         for(int i = 0; i < n; i++) {
             if(buffer[i] == '\n') {
                 line[line_len] = '\0';
@@ -57,17 +60,11 @@ void mem_monitor() {
                 line_len = 0;   //reset position
             }
             else {
-                if(line_len < sizeof(line_len) - 1) 
+                if(line_len < sizeof(line) - 1) 
                     line[line_len++] = buffer[i];
             }
         }
     }
-    
-    close(pipe1[0]);
-    close(pipe2[0]);
-    wait(NULL);
-    wait(NULL);
-
 }
 
 void parser(const char *line) {
